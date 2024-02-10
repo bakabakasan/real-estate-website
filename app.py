f226600fd06d08d15f6a4991474ec57da1ec2d64
bakabakasan <94224362+bakabakasan@users.noreply.github.com>
2024-02-10 13:13:32 +0000

    Добавлены HTML и CSS

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask #создаем объект класса фласк
+from flask import Flask, render_template #создаем объект класса фласк, добавляем методы для работы с html
 
 app = Flask(__name__)
 @app.route("/") #создание маршрута
 def hello_world(): #функция, которая будет вызвана при запросе к маршруту
-  return "Hello world!" #возвращаем строку
+  return render_template('home.html') #возвращаем файл html
 
 if __name__ == "__main__": #проверка на запуск самого скрипта 
   app.run(host="0.0.0.0", debug=True) #запуск приложения
